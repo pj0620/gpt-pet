a170e1555a66dad322bd1b03614dd66f694820e1
pj0620 <pjm7748@g.rit.edu>
2023-12-11 19:24:04 -0500

    add environment module

---
+++ modules/environment_module/constants/ai2thor_constants.py
@@ -0,0 +1,8 @@
+AI2THOR_MOVE_AHEAD = 'MoveAhead'
+AI2THOR_MOVE_BACK = 'MoveBack'
+AI2THOR_MOVE_LEFT = 'MoveLeft'
+AI2THOR_MOVE_RIGHT = 'MoveRight'
+
+AI2THOR_ROTATE_LEFT = 'RotateLeft'
+
+AI2THOR_NOP = 'Done'
