dc0ce32c4add6e081ee3f1f47f44a1f85b1d7d25
pj0620 <pjm7748@g.rit.edu>
2024-04-15 22:08:43 -0400

    startup script fix

--- start.sh
+++ start.sh
@@ -4,15 +4,15 @@
 if ! systemctl is-active --quiet docker; then
     echo "Docker daemon is not running. Starting Docker..."
     sudo systemctl start docker
-
-    # Wait for Docker to start and become ready
-    while ! systemctl is-active --quiet docker; do
-        echo "Waiting for Docker to start..."
-        sleep 1
-    done
 fi
 
-echo "Docker daemon is running."
+# Ensure Docker is fully operational
+while ! docker info > /dev/null 2>&1; do
+    echo "Waiting for Docker to be fully operational..."
+    sleep 1
+done
+
+echo "Docker daemon is running and ready."
 
 # Run docker-compose up
 echo "Running docker-compose up..."
