0304a5e6df2cc794423f0f288e6c6b535241bc6b
pj0620 <pjm7748@g.rit.edu>
2024-04-18 10:18:31 -0400

    add logs

--- service/sensor/physical/physical_proximity_sensor_adapter.py
+++ service/sensor/physical/physical_proximity_sensor_adapter.py
@@ -33,6 +33,7 @@ class PhysicalProximitySensorAdapter(BaseProximitySensorAdapter):
               self.measurements[direction].append(float(value))
               if len(self.measurements[direction]) > self.k:
                 self.measurements[direction].pop(0)
+      print(f"record_measurements: {self.measurements=}")
   
   def get_measurements(self) -> dict[str, str]:
     with self.lock:
