ccc5e42a864b84c4a9a2b63cf27eff849ff0694d
pj0620 <pjm7748@g.rit.edu>
2024-04-21 13:19:48 -0400

    move serial reading code into thread

--- service/sensor/physical/physical_proximity_sensor_adapter.py
+++ service/sensor/physical/physical_proximity_sensor_adapter.py
@@ -7,45 +7,43 @@ import threading
 import serial
 import time
 
-class PhysicalProximitySensorAdapter:
+from service.sensor.base_proximity_sensor_adapter import BaseProximitySensorAdapter
+
+
+class PhysicalProximitySensorAdapter(BaseProximitySensorAdapter):
     def __init__(self, k=5):
         self.k = k  # Number of measurements to keep
         self.measurements = {'ahead': [], 'back': [], 'right': [], 'left': []}
         self.lock = threading.Lock()  # Mutex for thread-safe access to measurements
 
-        # Initialize the serial port
-        self.initialize_serial()
-
         # Start the measurement recording thread
-        # self.thread = threading.Thread(target=self.record_measurements)
-        # self.thread.start()
+        self.thread = threading.Thread(target=self.record_measurements)
+        self.thread.start()
 
-    def initialize_serial(self):
+    def record_measurements(self):
         try:
-            self.serial_port = serial.Serial('/dev/ttyUSB0', 9600, timeout=2)
-            self.serial_port.flush()
+            serial_port = serial.Serial('/dev/ttyUSB0', 9600, timeout=2)
+            serial_port.flush()
         except serial.SerialException as e:
             print(f"Failed to open serial port: {e}")
-            raise
-
-    # def record_measurements(self):
-    #     while True:
-    #         try:
-    #             self.serial_port.readline()
-    #             line = self.serial_port.readline().decode('utf-8').strip()
-    #             print(f'Received line: {line}')
-    #             parts = line.split(',')
-    #             # Check if the line contains all four expected parts
-    #             if len(parts) == 4:
-    #                 with self.lock:
-    #                     # Update the measurements and maintain only the last 'k' measurements
-    #                     for direction, value in zip(['ahead', 'back', 'right', 'left'], parts):
-    #                         self.measurements[direction].append(float(value))
-    #                         if len(self.measurements[direction]) > self.k:
-    #                             self.measurements[direction].pop(0)
-    #                 print(f'Updated measurements: {self.measurements}')
-    #         except Exception as e:
-    #             print(f"Unexpected error in record_measurements: {e}")
+            raise e
+        
+        while True:
+            try:
+                line = serial_port.readline().decode('utf-8').strip()
+                print(f'Received line: {line}')
+                parts = line.split(',')
+                # Check if the line contains all four expected parts
+                if len(parts) == 4:
+                    with self.lock:
+                        # Update the measurements and maintain only the last 'k' measurements
+                        for direction, value in zip(['ahead', 'back', 'right', 'left'], parts):
+                            self.measurements[direction].append(float(value))
+                            if len(self.measurements[direction]) > self.k:
+                                self.measurements[direction].pop(0)
+                    print(f'Updated measurements: {self.measurements}')
+            except Exception as e:
+                print(f"Unexpected error in record_measurements: {e}")
 
     def get_measurements(self):
         with self.lock:
