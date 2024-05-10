e7c488235d7c54cccb4e268d9affbbbf3528e3a8
pj0620 <pjm7748@g.rit.edu>
2024-05-10 19:45:38 -0400

    change divide type to float for numpy in depth module

--- module/sensory/physical/physical_depth_camera_module.py
+++ module/sensory/physical/physical_depth_camera_module.py
@@ -14,6 +14,8 @@ class PhysicalDepthCameraModule(BaseSensoryModule):
     # Get depth data from the depth camera
     depth, _ = freenect.sync_get_depth()
     
+    print(depth)
+    print(f'type: {depth.dtype}')
     depth /= 1000.
     
     return {'last_depth_frame': depth}
