5d4ecd550e1328f9cee53666ea560ec41a04ede0
pj0620 <pjm7748@g.rit.edu>
2024-05-11 14:00:41 -0400

    change depth type to be aligned with rgb view

--- module/sensory/physical/physical_depth_camera_module.py
+++ module/sensory/physical/physical_depth_camera_module.py
@@ -8,11 +8,20 @@ import cv2
 # this will fail in local avoid importing unless installed from source
 import freenect
 
+# ref: https://stackoverflow.com/questions/12819599/freenect-depth-registered-has-no-effect-with-libfreenect
+FREENECT_DEPTH_10BIT = 1
+FREENECT_DEPTH_11BIT_PACKED = 2
+FREENECT_DEPTH_10BIT_PACKED = 3
+FREENECT_DEPTH_REGISTERED = 4
+FREENECT_DEPTH_MM = 5
+FREENECT_DEPTH_DUMMY = 2147483647
+
 class PhysicalDepthCameraModule(BaseSensoryModule):
   
   def build_subconscious_input(self, context: GPTPetContext) -> dict[str, Any]:
     # Get depth data from the depth camera
-    depth, _ = freenect.sync_get_depth()
+    # 4 ->
+    depth, _ = freenect.sync_get_depth(format=FREENECT_DEPTH_REGISTERED)
     
     depth = depth.astype('float64') / 1000.
     
