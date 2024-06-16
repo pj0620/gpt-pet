2cb663d7a492a7e938031ef6c7fe04ae48c8af0c
pj0620 <pjm7748@g.rit.edu>
2024-06-15 23:49:04 -0400

    add summarizer to gen agent conscious module

--- module/conscious/generative_agent_conscious_module.py
+++ module/conscious/generative_agent_conscious_module.py
@@ -4,21 +4,29 @@ from datetime import datetime
 from langchain.chains.llm import LLMChain
 from langchain.output_parsers import YamlOutputParser
 from langchain.retrievers import TimeWeightedVectorStoreRetriever
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate, SystemMessagePromptTemplate, \
   HumanMessagePromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_experimental.generative_agents import GenerativeAgentMemory
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 
 from gptpet_context import GPTPetContext
 from model.conscious import TaskDefinition, TaskResult, NewTaskResponse
 from module.conscious.base_conscious_module import BaseConsciousModule
+from service.analytics_service import AnalyticsService
 from service.vectordb_adapter_service import VectorDBAdapterService
 from utils.conscious import task_response_mapper, simple_subconscious_observation_summarizer
 from utils.prompt_utils import load_prompt, get_yaml
 
 
 class GenerativeAgentConsciousModule(BaseConsciousModule):
-  def __init__(self, vector_db_adapter_service: VectorDBAdapterService):
+  def __init__(
+      self,
+      vector_db_adapter_service: VectorDBAdapterService,
+      analytics_service: AnalyticsService
+  ):
+    self.analytics_service = analytics_service
     # setup gen agent memory
     retriever = TimeWeightedVectorStoreRetriever(
       vectorstore=vector_db_adapter_service.weaviate_memory_lc,
@@ -61,6 +69,16 @@ class GenerativeAgentConsciousModule(BaseConsciousModule):
       verbose=True
     )
     self.output_parser = YamlOutputParser(pydantic_object=NewTaskResponse)
+    
+    summarizer_prompt = ChatPromptTemplate.from_template(
+      load_prompt("conscious_genagent/summarize.txt")
+    )
+    self.summarizer_chain = (
+        {"observation": RunnablePassthrough()}
+        | summarizer_prompt
+        | ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+        | StrOutputParser()
+    )
   
   def generate_new_task(self, context: GPTPetContext) -> TaskDefinition:
     pprint.pprint({"conscious_inputs": [
@@ -115,11 +133,14 @@ class GenerativeAgentConsciousModule(BaseConsciousModule):
     """
     success_str = ""
     if not task_result.success:
-      success_str = "NOT"
+      success_str = "NOT "
       
-    final_memory_summary = (
-      f"After receiving the following context: {self.last_subconscious_summary}."
+    raw_observation_summary = (
+      f"After receiving the following context. {self.last_subconscious_summary}."
       f"GPTPet has decided to {task_definition.task} for the following reasoning. {task_definition.reasoning}."
-      f"This task was {success_str} executed successfully"
+      f"This task was {success_str}executed successfully"
     )
-    self.gen_memory.add_memory(final_memory_summary)
+    self.analytics_service.new_text(f"raw observation: {raw_observation_summary}")
+    compressed_memory_summary = self.summarizer_chain.invoke(raw_observation_summary)
+    self.analytics_service.new_text(f"compressed observation: {compressed_memory_summary}")
+    self.gen_memory.add_memory(compressed_memory_summary)
