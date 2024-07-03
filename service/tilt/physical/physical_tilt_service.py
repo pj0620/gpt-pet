75dd5cf1095ac06f142fb41c1983f9d818e9a2c9
pj0620 <pjm7748@g.rit.edu>
2024-07-03 00:32:43 -0400

    move tilt handler into freenect.runloop

--- service/tilt/physical/physical_tilt_service.py
+++ service/tilt/physical/physical_tilt_service.py
@@ -3,15 +3,29 @@ import freenect
 
 
 class PhysicalTiltService(BaseTiltService):
+  def __init__(self):
+    self.pending_tilt = False
+  
+  def body(self, dev, ctx, degrees):
+    if self.pending_tilt:
+      self.pending_tilt = False
+    else:
+      raise freenect.Kill
+    freenect.set_tilt_degs(dev, degrees)
+  
   def do_tilt(self, degrees: int) -> None:
     """Set the tilt angle of the Kinect sensor."""
     # The angle should be in the range of -30 to 30 degrees
     if -30 <= degrees <= 30:
-      freenect.sync_stop()
-      ctx = freenect.init()
-      dev = freenect.open_device(ctx, freenect.num_devices(ctx) - 1)
-      freenect.set_tilt_degs(dev, degrees)
-      freenect.sync_stop()
+      # freenect.sync_stop()
+      # ctx = freenect.init()
+      # dev = freenect.open_device(ctx, freenect.num_devices(ctx) - 1)
+      # freenect.set_tilt_degs(dev, degrees)
+      # freenect.sync_stop()
+      
+      self.pending_tilt = True
+      body_func = lambda dev, ctx: self.body(dev, ctx, degrees)
+      freenect.runloop(body=body_func)
       print(f"Tilt angle set to {degrees} degrees")
     else:
       print("Angle out of range. Please enter an angle between -30 and 30 degrees.")
