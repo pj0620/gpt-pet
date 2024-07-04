d845a478102a9b1f1c2951b064a29878cdb2a842
pj0620 <pjm7748@g.rit.edu>
2024-07-03 23:06:13 -0400

    combine tilt/led into one thread

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import os
-
 import numpy as np
 
-from constants.kinect import FREENECT_LED_BLINK_GREEN, FREENECT_LED_BLINK_RED_YELLOW, FREENECT_LED_YELLOW
+from constants.kinect import FREENECT_LED_YELLOW
 from gptpet import GPTPet
 from gptpet_context import GPTPetContext
 from mixin.goal.simple_chain_goal_mixin import SimpleChainGoalMixin
-from module.conscious.chain_conscious_module import ChainConsciousModule
-from module.conscious.generative_agent_conscious_module import GenerativeAgentConsciousModule
 from module.conscious.goal_aware_chain_conscious_module import GoalAwareChainConsciousModule
 from module.sensory.proximity_module import ProximityModule
 from module.sensory.sim.ai2thor_camera_module import Ai2ThorCameraModule
@@ -15,15 +11,14 @@ from module.sensory.sim.ai2thor_depth_camera_module import Ai2ThorDepthCameraMod
 from module.subconscious.input.base_subconscious_input_module import BaseSubconsciousInputModule
 from module.subconscious.input.proximiy_sensor_module import ProximitySensorModule
 from module.subconscious.input.stdin_speech_module import StdinAudioModule
-from module.subconscious.input.vision_module import VisionModule
 from module.subconscious.input.vision_module_with_goals import VisionModuleWithGoals
 from module.subconscious.output.single_input_agent_executor_module import SingleInputAgentExecutorModule
 from service.analytics_service import AnalyticsService
-from service.tilt_led.physical.physical_tilt_led_service import PhysicalLEDService, PhysicalTiltLedService
-from service.tilt_led.sim.noop_tilt_led_service import NoopLEDService, NoopTiltLedService
-from service.motor.sim.ai2thor_motor_adapter import Ai2ThorMotorService
 from service.device_io.sim.ai2thor_device_io_adapter import Ai2thorDeviceIOAdapter
+from service.motor.sim.ai2thor_motor_adapter import Ai2ThorMotorService
 from service.sim_adapter import SimAdapter
+from service.tilt_led.physical.physical_tilt_led_service import PhysicalTiltLedService
+from service.tilt_led.sim.noop_tilt_led_service import NoopTiltLedService
 from service.vectordb_adapter_service import VectorDBAdapterService
 from service.visual_llm_adapter_service import VisualLLMAdapterService
 from utils.env_utils import get_env_var, check_env_flag
@@ -49,7 +44,7 @@ if gptpet_env == 'local':
   
   context.analytics_service.new_text("initializing motor service")
   context.motor_adapter = Ai2ThorMotorService(sim_adapter)
-  context.led_service = NoopLEDService()
+  # context.led_service = NoopLEDService()
   context.led_tilt_service = NoopTiltLedService()
   
   context.analytics_service.new_text("initializing camera/depth camera modules")
@@ -69,7 +64,7 @@ elif gptpet_env == 'physical':
   
   context.analytics_service.new_text("initializing device io adapter")
   context.device_io_adapter = PhysicalDeviceIOAdapter()
-  context.led_service = PhysicalLEDService()
+  # context.led_service = PhysicalLEDService()
   context.led_tilt_service = PhysicalTiltLedService()
   
   context.analytics_service.new_text("initializing motor service")
